13676c3707ac9db35451aaad8ac658ef286d51ab
Jason Rosenblum <rosenblumj@ufl.edu>
2023-10-11 19:43:59 -0400

    Add pressure analysis folder and file

---
+++ pressure_analysis/pressure_analysis.py
@@ -0,0 +1,17 @@
+import numpy
+
+# List of variables here
+# - user inputs
+# - simplified variable equations
+
+# Start-up
+
+
+# Steady-state
+
+
+# Burn-down (or whatever this is called)
+
+
+# Data processing
+# - plots, etc.
