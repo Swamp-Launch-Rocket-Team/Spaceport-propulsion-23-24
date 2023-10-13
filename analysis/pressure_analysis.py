f83004b7de20083ed17646e5e9a2e162a90bdd47
Jason Rosenblum <rosenblumj@ufl.edu>
2023-10-13 15:11:08 -0400

    Restructure files, add separate analysis files

---
+++ analysis/pressure_analysis.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def chamber_pressure():
+    # this function should call the startup(), steady_state(), and tail_off() functions
+
+
+def startup():
+
+
+
+def steady_state():
+
+
+
+def tail_off():
+
+
+
+# Add other functions down here
---
+++ main.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+from analysis import nozzle_analysis as na, pressure_analysis as pa
+
+# Main code for analysis will go here
